b8d52b8c6cc5f073fb889975d6919d26758ef797
Jean Claude <aziaha.formations@gmail.com>
2023-10-18 16:51:10 +0200

    Système d'authentification en cours.

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -44,13 +44,14 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) 
+        {
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirection de l'utilisateur vers la page d'accueil
+        return new RedirectResponse($this->urlGenerator->generate('visitor.welcome.index'));
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
